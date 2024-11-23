dff1e44c94c57b39996cb3667e21dd4c3cd9e355
Tait Hoyem <tait@tait.tech>
2024-11-23 11:41:45 -0700

    Add max_depth and max_children stats

--- code/scripts/get_stats.sh
+++ code/scripts/get_stats.sh
@@ -1,5 +1,7 @@
 leafs=$(jq '[ recurse(.children[]?) | select(.children == [] )] | length' < "$1")
 nodes=$(jq '[ recurse(.children[]?) | .role ] | length' < "$1")
+max_depth=$(jq 'def max_depth: if type == "object" or type == "array" then (map(. | max_depth) | max) + 1 else 0 end; max_depth' < "$1")
+max_children=$(jq '[ recurse(.children[]?) | .children | length ] | max' < "$1")
 roles=($(jq '[ recurse(.children[]?) | .role ] | unique | .[]' < "$1" | tr -d '\n' | tr '""' ' ' | tr -d '"'))
 unique_roles=$(jq '[ recurse(.children[]?) | .role ] | unique | length' < "$1")
 declare -a each_role
@@ -14,3 +16,5 @@ echo "Unique Roles: $unique_roles"
 for i in "${!each_role[@]}"; do
 	echo -e "\t${roles[$i]}: ${each_role[$i]}"
 done
+echo "Max depth: $max_depth"
+echo "Max children: $max_children"
